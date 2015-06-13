7733c2c0209a0160262fa45e95f60a75e8b603ee
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2015-06-13 18:07:33 +0200

    Working OFX analysis

---
+++ JuloOFX/OFXUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace JuloOFX
+{
+    public class OFXUtils
+    {
+        public static DateTime ConvertOFXDateTimetoSysDateTime(string ofxDateTime)
+        {
+            string[] formats = { "yyyyMMddHHmmss.FFF", "yyyyMMddHHmmss", "yyyyMMddHHmm", "yyyyMMdd"};
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            return DateTime.ParseExact(ofxDateTime, formats, provider, DateTimeStyles.None);
+        }
+    }
+}
--- JuloOFXTest/Program.cs
+++ JuloOFXTest/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using JuloOFX;
 
 namespace JuloOFXTest
@@ -21,7 +23,22 @@ namespace JuloOFXTest
             }
 
             h.printHeaders();
-            Console.Out.WriteLine(c.xmlContents.ToString());
+            //c.xmlContents.Save(Console.Out);
+            XmlNode sonrs = c.xmlContents.SelectSingleNode("/OFX/SIGNONMSGSRSV1/SONRS");
+            XmlNode dtserver = sonrs.SelectSingleNode("DTSERVER");
+            string dtserverstr = dtserver.InnerText;
+            Console.WriteLine("DTSERVER " + dtserverstr);
+            DateTime d = JuloOFX.OFXUtils.ConvertOFXDateTimetoSysDateTime(dtserverstr);
+            Console.WriteLine(d.ToString());
+            XmlNode stmttrnrs = c.xmlContents.SelectSingleNode("/OFX/BANKMSGSRSV1/STMTTRNRS");
+            string transactionID = stmttrnrs.SelectSingleNode("TRNUID").InnerText;
+            Console.WriteLine("TRNUID " + transactionID);
+            XmlNodeList stmTrnList = c.xmlContents.SelectNodes("/OFX/BANKMSGSRSV1/STMTTRNRS/STMTRS/BANKTRANLIST/STMTTRN");
+            foreach (XmlNode stmTrn in stmTrnList)
+            {
+                Console.WriteLine("Transaction with " + stmTrn.SelectSingleNode("NAME").InnerText + " for amount " + stmTrn.SelectSingleNode("TRNAMT").InnerText);
+            }
+
             Console.ReadKey();
         }
     }
